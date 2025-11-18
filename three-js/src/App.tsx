c4f3a64db1bb254f50c99534a4149241e0d0bf73
yosefalemu <yosefalemu007@gmail.com>
2025-11-18 23:27:32 +0300

    refactor(App): integrate useEffect for scene setup and add OrbitControls

--- three-js/src/App.tsx
+++ three-js/src/App.tsx
@@ -1,40 +1,53 @@
+import { useEffect } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export default function App() {
-  console.log('THREE MODULE', THREE);
-  const scene = new THREE.Scene();
-  console.log('SCENE', scene);
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
+    const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    scene.add(cubeMesh);
 
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      3.9,
+      6
+    );
 
-  const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    const orthCamera = new THREE.OrthographicCamera(
+      -0.5 * aspectRatio,
+      0.5 * aspectRatio,
+      -0.5,
+      0.5,
+      0.1,
+      1000
+    );
+    orthCamera.position.z = 5;
+    camera.position.z = 5;
 
-  scene.add(cubeMesh);
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+    scene.add(orthCamera);
 
-  camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const canvasContainer = document.getElementById('canvas-container');
+    canvasContainer?.appendChild(renderer.domElement);
 
-  scene.add(camera);
-
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  const canvasContainer = document.getElementById('canvas-container');
-  console.log('CANVAS CONTAINER', canvasContainer);
-  canvasContainer?.appendChild(renderer.domElement);
-
-  function animate() {
-    window.requestAnimationFrame(animate);
-    cubeMesh.rotation.x += 0.01;
-    cubeMesh.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-  animate();
+    const orbit = new OrbitControls(orthCamera, renderer.domElement);
+    orbit.autoRotate = true;
+    orbit.autoRotateSpeed = 0.5;
+    function animate() {
+      requestAnimationFrame(animate);
+      // cubeMesh.rotation.x += 0.01;
+      // cubeMesh.rotation.y += 0.01;
+      orbit.update();
+      renderer.render(scene, orthCamera);
+    }
+    animate();
+  }, []);
 
   return <div id="canvas-container" />;
 }

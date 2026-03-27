686822702cd0d7ac3af4b0be505f44fd1f2d01fb
yosef <yosefalemu007@gmail.com>
2026-03-27 20:35:12 +0300

    feat(fastapi): implement item CRUD operations with pagination support and update requirements

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.routers.item import router as crud_router
+from app.routers.items import router as items_router
 from basic import router as basic_router
 from static import router as static_router
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(crud_router)
+app.include_router(items_router)
 app.include_router(basic_router)
 app.include_router(static_router)
 

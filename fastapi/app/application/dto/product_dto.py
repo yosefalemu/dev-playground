dd6851add55aec2b8c59cb1fddb1e67a05cb40b6
yosef <yosefalemu007@gmail.com>
2026-04-01 16:42:56 +0300

    feat(fastapi): update product service to use new DTOs for product responses and enhance product retrieval methods

--- fastapi/app/application/dto/product_dto.py
+++ fastapi/app/application/dto/product_dto.py
@@ -1,6 +1,15 @@
-from app.domain.entities.product import Product
-
+class ProductResponseDTO:
+    def __init__(self, id: int, name: str, stock: int,price: float):
+        self.id = id
+        self.name = name
+        self.stock = stock
+        self.price = price
+class ProductResponseForUserDTO:
+    def __init__(self, id: int, name: str, price: float):
+        self.id = id
+        self.name = name
+        self.price = price
 class ProductListResponseDTO:
-    def __init__(self, products: list[Product], count: int):
+    def __init__(self, products: list[ProductResponseForUserDTO], count: int):
         self.products = products
         self.count = count
--- fastapi/app/application/services/product_service.py
+++ fastapi/app/application/services/product_service.py
@@ -1,4 +1,4 @@
-from app.application.dto.product_dto import ProductListResponseDTO
+from app.application.dto.product_dto import ProductListResponseDTO, ProductResponseDTO, ProductResponseForUserDTO
 from app.domain.exceptions.product_exc import ProductAlreadyExistsException, ProductNotFoundException, ProductOutofStockException, InsufficientStockException
 from app.domain.repositories.product_repository import ProductRepository
 from typing import Optional,cast
@@ -7,30 +7,43 @@ from app.domain.entities.product import Product
 class ProductService:
     def __init__(self, product_repository: ProductRepository):
         self.product_repository = product_repository
+    
     async def get_products(self) -> ProductListResponseDTO:
         products, count = await self.product_repository.get_products()
-        return ProductListResponseDTO(products=products, count=count)
-    async def get_product_by_id(self, product_id: int) -> Optional[Product]:
+        products = cast(list[Product], products)
+        product_dtos = [
+            ProductResponseForUserDTO(
+                id=product.id,
+                name=product.name,
+                price=product.price
+            )
+            for product in products
+        ]
+        return ProductListResponseDTO(products=product_dtos, count=count)
+    
+    async def get_product_by_id(self, product_id: int) -> Optional[ProductResponseForUserDTO]:
         product_found = await self.product_repository.get_product_by_id(product_id=product_id)
         if not product_found:
             raise ProductNotFoundException(f"Product with id {product_id} not found")
-        return product_found
-    async def get_product_by_name(self, name: str) -> Optional[Product]:
+        product_found = cast(Product, product_found)
+        return ProductResponseForUserDTO(
+            id=product_found.id,
+            name=product_found.name,
+            price=product_found.price
+        )
+    
+    async def get_product_by_name(self, name: str) -> Optional[ProductResponseForUserDTO]:
         product_found = await self.product_repository.get_product_by_name(name=name)
         if not product_found:
             raise ProductNotFoundException(f"Product with name {name} not found")
-        return product_found
-    async def create_product(self, product: Product) -> Product:
-        existing_product_by_name = await self.product_repository.get_product_by_name(name=product.name)
-        if existing_product_by_name:
-            raise ProductAlreadyExistsException(f"Product with name {product.name} already exists")
-        return await self.product_repository.create_product(product=product)
-    async def delete_product(self, product_id: int) -> Optional[Product]:
-        deleted_product = await self.product_repository.delete_product(product_id=product_id)
-        if not deleted_product:
-            raise ProductNotFoundException(f"Product with id {product_id} not found")
-        return deleted_product
-    async def purchase_product(self, product_id: int, quantity: int) -> Product:
+        product_found = cast(Product, product_found)
+        return ProductResponseForUserDTO(
+            id=product_found.id,
+            name=product_found.name,
+            price=product_found.price
+        )
+    
+    async def purchase_product(self, product_id: int, quantity: int) -> ProductResponseForUserDTO:
         product_found = await self.product_repository.get_product_by_id(product_id=product_id)
         if not product_found:
             raise ProductNotFoundException(f"Product with id {product_id} not found")
@@ -39,9 +52,32 @@ class ProductService:
             raise ProductOutofStockException(f"Product with id {product_id} is out of stock")
         if product_found.stock < quantity:
             raise InsufficientStockException(f"Product with id {product_id} has insufficient stock")
-        return await self.product_repository.purchase_product(product_id=product_id, quantity=quantity)
+        purchased_product = await self.product_repository.purchase_product(product_id=product_id, quantity=quantity)
+        if not purchased_product:
+            raise ProductNotFoundException(f"Product with id {product_id} not found")
+        return ProductResponseForUserDTO(
+            id=purchased_product.id,
+            name=purchased_product.name,
+            price=purchased_product.price
+        )
     
-    async def increase_stock(self, product_id: int, quantity: int) -> Product:
+
+    # For the admin to create, delete and increase stock of products
+    # I will implement authentication and authorization later, for now I will just implement the functionality without any authentication and authorization
+    async def create_product(self, product: Product) -> ProductResponseDTO:
+        existing_product_by_name = await self.product_repository.get_product_by_name(name=product.name)
+        if existing_product_by_name:
+            raise ProductAlreadyExistsException(f"Product with name {product.name} already exists")
+        return await self.product_repository.create_product(product=product)
+    
+    async def delete_product(self, product_id: int) -> Optional[ProductResponseDTO]:
+        deleted_product = await self.product_repository.delete_product(product_id=product_id)
+        if not deleted_product:
+            raise ProductNotFoundException(f"Product with id {product_id} not found")
+        return deleted_product
+    
+
+    async def increase_stock(self, product_id: int, quantity: int) -> ProductResponseDTO:
         product_found = await self.product_repository.get_product_by_id(product_id=product_id)
         if not product_found:
             raise ProductNotFoundException(f"Product with id {product_id} not found")
